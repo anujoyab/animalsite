f331f182c0532b17a9ccbfa4bf49e8d1a2f18f48
Anujoy Biswas <154691691+anujoyab@users.noreply.github.com>
2025-02-02 12:35:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+document.getElementById("btn btn-primary").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    alert("Thank you for your interest! The British Shorthair is added to your cart.");
+  });
+
+
+
+
+
+
+
+
+
